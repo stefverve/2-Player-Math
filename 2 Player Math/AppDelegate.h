1b5bbd795491f3af2aef48a2d8d5eb391c61ea53
stef <stef.verve@gmail.com>
2016-11-07 14:41:00 -0500

    Initial Commit

---
+++ 2 Player Math/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2 Player Math
+//
+//  Created by Stefan Verveniotis on 2016-11-07.
+//  Copyright © 2016 Stefan Verveniotis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2 Player Math/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2 Player Math
+//
+//  Created by Stefan Verveniotis on 2016-11-07.
+//  Copyright © 2016 Stefan Verveniotis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
